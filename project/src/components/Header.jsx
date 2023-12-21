d6c4c188dafb4156bfd4c3282ab45d0a09d96deb
Ujjwal Puri <ujjwalpuri221@gmail.com>
2023-12-21 16:33:15 +0545

    added header

---
+++ project/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function Header(){
+    return(
+        <div>
+            <h1 className='flex justify-center text-3xl bg-green-200 p-14'>CV Application</h1>
+        </div>
+    )
+}
